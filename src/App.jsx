ae555824390780d0cdcdf00f24c70ae6c9c85dbd
SEBASTIAN HERRERA <ecoshop863@gmail.com>
2026-04-24 12:51:12 -0500

    Cree la página de Contacto y instale el react-router-dom, además se arreglaron las rutas y se dejo funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Inicio from "./pages/Inicio"
+import Productos from "./pages/Productos"
+import Contacto from "./pages/Contacto"
+
 export default function App() {
     return (
-        <h1 className="text-4xl font-bold text-blue-500">
-    Tailwind funcionando
-        </h1>)
+        <BrowserRouter>
+            <div className="min-h-screen bg-[#111111] text-white">
+                <Navbar />
+
+                <Routes>
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/productos" element={<Productos />} />
+                    <Route path="/contacto" element={<Contacto />} />
+                </Routes>
+
+                <Footer />
+            </div>
+        </BrowserRouter>
+    )
 }
---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,5 @@
+import Contact from "../components/Contact.jsx"
+
+export default function Contacto() {
+    return <Contact />
+}
